b0983469d955113d490c783db0ff886646bbea55
Siddisaai_Gunji <siddisaai_gunji@epam.com>
2023-08-16 15:15:28 +0530

    Initial Commit

---
+++ src/main/java/com/epam/calcjenkinstest/CalcJenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package com.epam.calcjenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcJenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcJenkinsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/calcjenkinstest/CalcJenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.calcjenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcJenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
