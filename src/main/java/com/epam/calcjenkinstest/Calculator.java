c98908552ba93880d4ac12b3c9c1f44b6baf759a
Siddisaai_Gunji <siddisaai_gunji@epam.com>
2023-08-16 15:31:10 +0530

    Initial Commit

--- src/main/java/com/epam/calcjenkinstest/Calculator.java
+++ src/main/java/com/epam/calcjenkinstest/Calculator.java
@@ -5,17 +5,32 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Calculator {
 
+
+
+
     public int addTwoNumbers(int num1,int num2) {
+        log.info("addTwoNumbers method invoked");
+        log.info("Hello..");
+        log.info("addTwoNumbers method invoked");
+        log.info("Hello..");
         log.info("addTwoNumbers method invoked");
         log.info("Hello..");
         return num1+num2;
     }
     public int subtractTwoNumbers(int num1,int num2) {
+        log.info("addTwoNumbers method invoked");
+        log.info("Hello..");
+        log.info("addTwoNumbers method invoked");
+        log.info("Hello..");
         log.info("subtractTwoNumbers method invoked");
         return num1-num2;
     }
 
     public int multiplyTwoNumbers(int num1,int num2) {
+        log.info("addTwoNumbers method invoked");
+        log.info("Hello..");
+        log.info("addTwoNumbers method invoked");
+        log.info("Hello..");
         log.info("multiplyTwoNumbers method invoked");
         return num1 * num2;
     }
